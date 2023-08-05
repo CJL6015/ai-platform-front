e77dc3d5afd7da44c4248c3a51ee5031de2a7551
Jiale <cjl601523@gmail.com>
2023-08-05 14:08:42 +0800

    feat: ✨ 修改菜单路由及其他基础配置

---
+++ src/locales/lang/zh-CN/routes/warn.ts
@@ -0,0 +1,6 @@
+export default {
+  title: '预警中心',
+  history: '历史数据',
+  photo: '巡检抓拍',
+  rule: '预警规则',
+};
---
+++ src/router/routes/modules/warn.ts
@@ -0,0 +1,47 @@
+import type { AppRouteModule } from '/@/router/types';
+
+import { LAYOUT } from '/@/router/constant';
+import { t } from '/@/hooks/web/useI18n';
+
+const warn: AppRouteModule = {
+  path: '/warn',
+  name: '模型中心',
+  component: LAYOUT,
+  redirect: '/warn/history',
+  meta: {
+    orderNo: 10,
+    icon: 'ion:grid-outline',
+    title: t('routes.warn.title'),
+  },
+  children: [
+    {
+      path: 'history',
+      name: 'history',
+      component: () => import('/@/views/warn/history/index.vue'),
+      meta: {
+        // affix: true,
+        title: t('routes.warn.history'),
+      },
+    },
+    {
+      path: 'photo',
+      name: 'photo',
+      component: () => import('/@/views/warn/photo/index.vue'),
+      meta: {
+        // affix: true,
+        title: t('routes.warn.photo'),
+        showInMenu: false,
+      },
+    },
+    {
+      path: 'rule',
+      name: 'rule',
+      component: () => import('/@/views/warn/rule/index.vue'),
+      meta: {
+        title: t('routes.warn.rule'),
+      },
+    },
+  ],
+};
+
+export default warn;
