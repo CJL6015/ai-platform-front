ff8bfa7283f130f20bb4c5834f4f48eb4c6fc1c0
Jiale <cjl601523@gmail.com>
2023-08-05 23:20:42 +0800

    feat: ✨ 新增巡检配置页面

---
+++ src/views/warn/photo/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <PageWrapper title="模型中心" contentFullHeight>
+    <a-card> <UnitSelect /> <Manage /><PhotoCard /> </a-card>
+  </PageWrapper>
+</template>
+<script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import { PageWrapper } from '/@/components/Page';
+  import UnitSelect from '../components/UnitSelect.vue';
+  import Manage from './Manage.vue';
+  import { Card } from 'ant-design-vue';
+  import PhotoCard from './PhotoCard.vue';
+
+  const loading = ref(true);
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+
+  export default defineComponent({
+    components: {
+      PageWrapper,
+      UnitSelect,
+      PhotoCard,
+      ACard: Card,
+      Manage,
+    },
+    setup() {
+      return {
+        loading,
+      };
+    },
+  });
+</script>
